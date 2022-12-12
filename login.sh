fa7bb0bf8a9d1af475b7f0b173e61166c9971d4b
Adel Mardini <mardiniadel5@gmail.com>
2022-12-12 11:24:43 +0300

    test test

---
+++ login.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+#sudo apt install openssh-server -y
+#sudo ufw allow ssh
+# sudo ssh amardini@192.168.223.128 "( cd Desktop/ && chmod 777 GS_Menu.sh && scp GS_Menu.sh amardini@192.168.223.129:/home/amardini/Desktop )"
+scp amardini@192.168.223.128:/home/amardini/Desktop/GS_Menu.sh /home/amardini/Desktop
